dd5ca166410da15ce0fd68292d3a19270c8ca404
Uche <chukwuanu@hotmail.de>
2022-08-17 15:13:19 +0200

    portfolio home page

---
+++ backend/script.js
@@ -0,0 +1,24 @@
+//Variables = Similar to a Pokémon Ball
+studentName = "Ananswer" /*String*/
+score = 9.7 /*Float*/
+payrollCurrency = "$" /*Character*/
+inZoomClass = true /*Boolean true|false*/
+number = 72 /*Integer*/
+
+console.log("This is a Simple Log")
+
+console.log("Student name is:", studentName, "they have the score in Html/Css of:", score)
+
+function rePaint(){
+    console.error("This is an Error");
+    alert("this is the Alert Function");
+    console.log(Math.random() * 12);
+    document.getElementById("body").style.color = "red";
+}
+
+funtion randomColor(){
+    console.log("This is the new Funtion")
+}
+function randomColor(){
+    return "#" + Math.floor(Math.random() * 16777215.toString(16);
+}
